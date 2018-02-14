81d23171c7d0a2416e54208cdcef1be4c27b14c8
Reinaldo Rossetti <reinaldo.rossetti@outlook.com>
2018-02-14 11:22:37 -0200

    Create uninstall_explorer11.bat

---
+++ uninstall_explorer11.bat
@@ -0,0 +1,2 @@
+
+FORFILES /P %WINDIR%\servicing\Packages /M Microsoft-Windows-InternetExplorer-*11.*.mum /c "cmd /c echo Uninstalling package @fname && start /w pkgmgr /up:@fname /quiet /norestart"
